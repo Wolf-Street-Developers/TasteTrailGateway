1917a996d4dc6b1fa13a66b0b45a2c530a7f16a7
makedoniaz <jafar.sadig04@gmail.com>
2024-09-14 23:54:23 +0400

    Add CORS policy

--- TasteTrailGateway/src/TasteTrailGateway.Api/Program.cs
+++ TasteTrailGateway/src/TasteTrailGateway.Api/Program.cs
@@ -3,6 +3,17 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin()  // Разрешить всем источникам
+                   .AllowAnyMethod()  // Разрешить все HTTP методы
+                   .AllowAnyHeader(); // Разрешить любые заголовки
+        });
+});
+
 builder.Configuration.AddJsonFile("ocelot.json", false, reloadOnChange: true);
 builder.Services.AddOcelot();
 
@@ -11,6 +22,8 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
 var app = builder.Build();
+
+app.UseCors("AllowAll");
 
 app.UseSwagger();
 app.UseSwaggerUI();
